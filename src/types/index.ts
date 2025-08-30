ab1d1bb261ab02556920a0a6dc87320fbd975c7a
Fiodorova Maria <mariya.sg.rp@gmail.com>
2025-08-30 00:02:35 -0500

    fix: добавила больше примеров с примерами вызовов методов реализованнх классов. Также исправила метод addProductToCart в классе catalog, чтобы можно было чейнить вызовы

--- src/types/index.ts
+++ src/types/index.ts
@@ -57,14 +57,31 @@ const products = getRespone.items;
 
 // передача массив товаров в Catalog
 catalog.setProductsList(products);
-
-// вывод в консоль списка товаров
 console.log("Список товаров из каталога:", catalog.getAllProducts());
+catalog.setSelectedProduct(catalog.getAllProducts()[0]);
+console.log(
+  "Пример выбранного товара, который должен отобразиться в модальном окне",
+  catalog.getSelectedProduct()
+);
 
 const cart = new Cart();
-cart.addProductToCart(catalog.getAllProducts()[0]);
-console.log(cart.getCartProducts());
-console.log(cart.getProductsQuantity());
+cart
+  .addProductToCart(catalog.getAllProducts()[0])
+  .addProductToCart(catalog.getAllProducts()[1])
+  .addProductToCart(catalog.getAllProducts()[2])
+  .addProductToCart(catalog.getAllProducts()[3]);
+console.log("Товары, добавленные в корзину:", cart.getCartProducts());
+console.log("Количество товаро в корзине:", cart.getProductsQuantity());
+console.log("Общая стоимость товаров в корзине:", cart.getTotalCost());
+cart.deleteProductFromCart(
+  catalog.getProductById(catalog.getAllProducts()[3]["id"])
+);
+console.log(
+  "Товары в корзине после удаления одного из товаров",
+  cart.getCartProducts()
+);
+cart.clearCart();
+console.log("Корзина после очищения:", cart.getCartProducts());
 
 const customer = new Customer();
 customer
@@ -72,4 +89,4 @@ customer
   .setField("email", "email@domain.com")
   .setField("phone", "1234567890")
   .setPaymentMethod("card");
-console.log(customer.getCustomerData());
+console.log("Данные о покупателе", customer.getCustomerData());
